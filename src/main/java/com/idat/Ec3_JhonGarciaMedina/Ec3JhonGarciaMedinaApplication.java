588b67350e8d817ecfb4d8c2426450a1c14791a5
jhongarciamedina <jhonfarru.game@gmail.com>
2022-07-16 20:22:35 -0500

    subiendo proyecto

---
+++ src/main/java/com/idat/Ec3_JhonGarciaMedina/Ec3JhonGarciaMedinaApplication.java
@@ -0,0 +1,13 @@
+package com.idat.Ec3_JhonGarciaMedina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec3JhonGarciaMedinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec3JhonGarciaMedinaApplication.class, args);
+	}
+
+}
